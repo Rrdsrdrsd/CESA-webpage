71c745f52db027aafd3b244638eb908189160f80
Affan <khamseaffan@gmail.com>
2022-06-25 12:44:38 +0530

    first commit files generated

---
+++ src/Containers/Navbar/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+
+export default function Header() {
+  return <h1>./Containers/Navbar</h1>;
+}
---
+++ src/Pages/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Clubname from "../Containers/ClubName/clubName";
+
+export default function Landing() {
+  return (
+    <div>
+      <Clubname />
+    </div>
+  );
+}
